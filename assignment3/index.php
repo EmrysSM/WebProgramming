83a8e32ef8efd01fe93fbf6c5fc1a0e334713dfe
Emrys Scott-Murrell <escottmurrell1@student.gsu.edu>
2018-06-26 14:18:57 -0400

    Started index php for assignment

---
+++ assignment3/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Assignment 3</title>
+</head>
+<body>
+    <?php
+    /**
+     * Assignment 3 for Web Programming
+     * Emrys Scott-Murrell
+     * Date: 6/26/18
+     */
+
+
+    ?>
+</body>
+</html>
