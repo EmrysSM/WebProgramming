5fbd0844ed61d1776e0f4e335c6aa2ffbb5d90cd
Emrys Scott-Murrell <escottmurrell1@student.gsu.edu>
2018-06-27 15:03:42 -0400

    calculate the average of three numbers in php

---
+++ inclass.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>In Class</title>
+</head>
+<body>
+<?php
+
+function avgOfThree($a, $b, $c) {
+    return ($a + $b + $c) / 3;
+}
+
+$answer = avgOfThree(1, 2, 3);
+
+echo "<p>$answer</p>";
+?>
+</body>
+</html>
+
+
+
