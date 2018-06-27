10beaec64826ad3338e00ce3e90754f15339a045
Emrys Scott-Murrell <escottmurrell1@student.gsu.edu>
2018-06-27 14:48:59 -0400

    starting part 4

--- assignment3/index.php
+++ assignment3/index.php
@@ -92,7 +92,10 @@
         echo "</p>";
 
         // Part 4
-
+        $restaurants = array("Chama Gaucha"=>"40.50", "Aviva by Kameel"=>"15.00",
+            "Bone's Restaurant"=>"65.00", "Umi Sushi Buckhead"=>"40.50", "Fandangles"=>"30.00",
+            "Capital Grill"=>"60.50", "Canoe"=>"35.50", "One Flew South"=>"21.00",
+            "Fox Bros. BBQ"=>"15.00", "South City Kitchen Midtown"=>"29.00")
 
     ?>
 </body>
