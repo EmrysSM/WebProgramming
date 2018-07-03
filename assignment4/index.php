f5f3345cf3ac8974fcdc5401f448556f34b4f469
Emrys Scott-Murrell <escottmurrell1@student.gsu.edu>
2018-07-03 15:00:37 -0400

    starting part 1 of assignment 4

---
+++ assignment4/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Assignment 4</title>
+  </head>
+  <body>
+    <form action="index.php">
+        <textarea name="tarea" rows="20" cols="30"></textarea>
+        <input type="submit" value="submit">
+    </form>
+    <?php
+
+    ?>
+  </body>
+</html>
