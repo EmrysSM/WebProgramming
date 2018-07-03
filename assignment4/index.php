b994957b458267e07ece099d97d0cbd6d0299a44
Emrys Scott-Murrell <escottmurrell1@student.gsu.edu>
2018-07-03 16:49:19 -0400

    finished form for part 1 of assignment 4

--- assignment4/index.php
+++ assignment4/index.php
@@ -3,10 +3,33 @@
   <head>
     <meta charset="UTF-8">
     <title>Assignment 4</title>
+      <link rel="stylesheet" type="text/css" href="assignment4.css">
   </head>
   <body>
     <form action="index.php">
-        <textarea name="tarea" rows="20" cols="30"></textarea>
+        <div class="formGroup">
+            <label>Select a Background Color:</label>
+            <select name="bgcolor">
+                <option value="blk">Black</option>
+                <option value="wht">White</option>
+                <option value="yel">Yellow</option>
+            </select>
+        </div>
+        <div class="formGroup">
+            <label> Select a font color:</label>
+            <div class="formElements">
+                <input type="radio" name="fcolor" value="blk">Black<br>
+                <input type="radio" name="fcolor" value="wht">White<br>
+                <input type="radio" name="fcolor" value="grn">Green<br>
+            </div>
+        </div>
+        <div class="formGroup">
+            <label>Select if Italic:</label>
+            <input type="checkbox" name="italic" value="ital">
+        </div>
+        <div class="formGroup">
+            <textarea name="tarea" rows="10" cols="30">Enter the text for the page</textarea>
+        </div>
         <input type="submit" value="submit">
     </form>
     <?php
