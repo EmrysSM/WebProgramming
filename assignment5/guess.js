4590ac548d6d0d960f04f5f0d2fe879e5cd16977
emryssm <emrys.sm@gmail.com>
2018-07-20 16:57:55 -0400

    finished part b of assignment 5

--- assignment5/guess.js
+++ assignment5/guess.js
@@ -1,13 +1,54 @@
 var number = Math.floor(Math.random() * 100) + 1;
+var guessBtn = document.getElementById("guessBtn");
+var newBtn = document.getElementById("newBtn");
+var limit = 10;
+var turn = 0;
 
-document.getElementById("guessBtn").onclick = function() {
-    var guess = document.getElementById("guess").value;
-    if (guess < number) {
-        document.getElementById("feedback").innerHTML = "You guessed too low!"
-    } else if (guess > number) {
-        document.getElementById("feedback").innerHTML = "You guessed too high!"
+guessBtn.onclick = function() {
+    if (turn < limit) {
+        turn++;
+        var guess = document.getElementById("guess").value;
+        if (guess < number) {
+            document.getElementById("feedback").innerHTML = "You guessed too low!";
+            document.getElementById("turn").innerHTML = "" + (limit - turn);
+            document.getElementById("audio").play();
+        } else if (guess > number) {
+            document.getElementById("feedback").innerHTML = "You guessed too high!";
+            document.getElementById("turn").innerHTML = "" + (limit - turn);
+            document.getElementById("audio").play();
+        } else {
+            document.getElementById("feedback").innerHTML = "You guessed right!";
+            document.getElementById("turn").innerHTML = "" + (limit - turn);
+            document.getElementById("winAudio").play();
+            guessBtn.style.display = "none";
+            newBtn.style.display = "inline";
+        }
     } else {
-        document.getElementById("feedback").innerHTML = "You guessed right!"
-        // hide the guess button and show a new game button
+        document.getElementById("feedback").innerHTML = "You ran out of turns!"
+        document.getElementById("winAudio").play();
+        guessBtn.style.display = "none";
+        newBtn.style.display = "inline";
+    }
+}
+
+newBtn.onclick = function () {
+    location.reload();
+}
+
+var secondsLabel = document.getElementById("seconds");
+var totalSeconds = 0;
+setInterval(setTime, 1000);
+
+function setTime() {
+    ++totalSeconds;
+    secondsLabel.innerHTML = pad(totalSeconds % 60);
+}
+
+function pad(val) {
+    var valString = val + "";
+    if (valString.length < 2) {
+        return "0" + valString;
+    } else {
+        return valString;
     }
 }
---
+++ assignment5/match.js
@@ -0,0 +1,3 @@
+document.getElementById("number").onchange = function () {
+
+}
